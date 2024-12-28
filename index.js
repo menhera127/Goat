65eb5c2c46cc0351c7133362313c4923f26e34c7
Gerald max <166443104+13065994@users.noreply.github.com>
2024-12-28 17:29:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
 /** 
- * @author NTKhang
+ * @author NTKhang 
  * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using 
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2 
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked 
@@ -12,7 +13,8 @@
  * 
  * Vietnamese: 
  * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án. 
- *2222* ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn 
+ * Nó là động lực để tôi duy trì và phát triển dự án miễn phí. 
+ * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn 
  * Cảm ơn bạn đã sử dụng 
  */
 
@@ -22,37 +24,35 @@ const log = require("./logger/log.js");
 const app = express();
 const config = require("./config.dev.json");
 const PORT = process.env.PORT || config.port || 3000;
-const deploymentUrl = process.env.RENDER_EXTERNAL_URL ||`https:${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
 
 app.get("/", (req, res) => {
     res.send("Project is running.");
 });
 
 function startProject() {
-    const child = spawn("node", ["Goat.js"], {
-        cwd: __dirname, 
-        stdio: "inherit", 
-        shell: true 
+    const child = spawn("node", ["Goat.js"], { 
+	    cwd: __dirname, 
+	    stdio: "inherit", 
+	    shell: true 
     });
 
     child.on("close", (code) => {
         if (code === 2) {
             const ran = "\u0054\u0068\u0061\u0062\u0061\u006e\u0069";
-            log.info(`[INFO]: PROJECT RESTART ... prod ${ran}`);
+            log.info(`[»] PROJECT RESTART ... prod ${ran}`);
             startProject();
         } else {
-            log.error(`[ERROR]: PROJECT EXITED WITH CODE: ${code}`);
-            process.exit(1);
+            log.error(`[»] PROJECT EXITED WITH CODE: ${code}`);
         }
     });
 
     child.on("error", (err) => {
-        log.error(`[ERROR]: Failed to start child process: ${err.message}`);
+        log.error(`[»] Failed to start child process: ${err.message}`);
     });
 }
 
 app.listen(PORT, () => {
-    console.log(`==> [INFO]: port connection is successful.\nPORT: ${PORT}\n`);
-}
+    console.log(`[»] HOSTED: http://localhost:${PORT}\nUPTIME THIS URL SITE: https://${process.env.RENDER_EXTERNAL_URL || `${process.env.REPL_OWNER}.${process.env.REPL_SLUG}.repl.app`}`);
+});
 
 startProject();
